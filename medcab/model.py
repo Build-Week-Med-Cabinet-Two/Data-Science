34ff40372d245a45537e76c222d29e1a168ca8f3
CurdtMillion <curtcecil@gmail.com>
2020-08-20 00:23:47 -0500

    Added model file w/ startup db column names

--- medcab/model.py
+++ medcab/model.py
@@ -3,3 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Should we use the column names etc from the kaggle db?
+
+class PlantInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    strain = db.Column(db.String(80), unique=True, nullable=False)
+    planttype = db.Column(db.String(80), unique=True, nullable=False)
+    rating = db.Column(db.Float, unique=True, nullable=False)
+    effects = db.Column(db.String(80), unique=True, nullable=False)
+    flavor = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<PlantInfo %r>' % self.username
+
