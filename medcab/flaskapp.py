83bb1d039a07a3ce8599ec9a49799e4d51a40ce7
CurdtMillion <curtcecil@gmail.com>
2020-08-20 18:40:10 -0500

    Added cannabis.csv from kaggle, created db.py to test connection and queries

--- medcab/flaskapp.py
+++ medcab/flaskapp.py
@@ -9,6 +9,7 @@ def create_app():
     '''Create and configure an instance of the Flask application'''
 
     app = Flask(__name__)
+    app.config['DEBUG'] = True
     app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')  # <---- work on getting the URL corrected
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
